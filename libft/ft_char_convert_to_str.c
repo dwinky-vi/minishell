78b0a18117440b51f94f912c34e5e3ff651f7a05
Darlena Winky <dwinky@et-h6.kzn.21-school.ru>
2021-04-17 22:53:17 +0300

    new: ft_char_convert_to_str.c

---
+++ libft/ft_char_convert_to_str.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_char_convert_to_str.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwinky <dwinky@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/17 22:49:57 by dwinky            #+#    #+#             */
+/*   Updated: 2021/04/17 22:51:55 by dwinky           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/** This function allocate memory for str */
+char	*char_convert_to_str(char ch)
+{
+	char *str;
+
+	str = (char *)ft_calloc(2, sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[0] = ch;
+	return (str);
+}
